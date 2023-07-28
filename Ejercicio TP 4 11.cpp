c1340095c681dd29f56f5ac2215a89674f93f441
PoLeNQuE <136858206+PoLeNQuE@users.noreply.github.com>
2023-07-28 20:15:55 -0300

    Add files via upload

---
+++ Ejercicio TP 4 11.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main() {
+	int i;
+	char cadena[100];
+	int largo = 0, vocales = 0;
+	
+	printf("Ingrese la cadena que desea escanear: ");
+	scanf("%99[^\n]", cadena); 
+	
+	while (cadena[largo] != '\0') {
+		
+		if (cadena[largo] == 'a'){
+			vocales++;
+		}
+		else if (cadena[largo] == 'A'){
+			vocales++;
+		}
+		else if (cadena[largo] == 'e'){
+			vocales++;
+		}
+		else if (cadena[largo] == 'E'){
+			vocales++;
+		}
+		else if (cadena[largo] == 'i'){
+			vocales++;
+		}
+		else if (cadena[largo] == 'I'){
+			vocales++;
+		}
+		else if (cadena[largo] == 'o'){
+			vocales++;
+		}
+		else if (cadena[largo] == 'O'){
+			vocales++;
+		}
+		else if (cadena[largo] == 'u'){
+			vocales++;
+		}
+		else if (cadena[largo] == 'U'){
+			vocales++;
+		}
+		else{}
+		largo++;
+	}
+	
+	printf("\nLa cantidad de vocales que hay en la cadena son: %d", vocales);
+	return 0;
+}
---
+++ Ejercicio TP 4 12.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+	int i, k;
+	char cadena[100];
+	int largo = 0, vocales = 0, maximolargo;
+	
+	printf("Ingrese la cadena que desea encriptar: ");
+	scanf("%99[^\n]", cadena); 
+	
+	while (cadena[largo] != '\0') {
+		
+		cadena[largo] = (cadena[largo] + 3);
+		maximolargo = largo;
+		largo++;
+	}
+	
+	printf("\nEl mensaje encriptado es: %s", cadena);
+	return 0;
+}
