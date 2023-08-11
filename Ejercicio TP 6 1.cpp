15297fc67439bca2f4df2cbd42d9a35ddc6c528b
PoLeNQuE <136858206+PoLeNQuE@users.noreply.github.com>
2023-08-11 18:47:04 -0300

    Add files via upload

---
+++ Ejercicio TP 6 1.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+using namespace std;
+int convPol_Rec(float mod, float ang, float *real, float *imaginario){
+	*real= mod*cos(ang);
+	*imaginario = mod*sin(ang);
+	return 0;
+}
+int convRec_Pol(float real, float imaginario,float *mod, float *ang){
+	*mod = sqrt(real*real+imaginario*imaginario);
+	*ang = atan(imaginario/real);
+	return 0;
+}
+int main() {
+	int e;
+	float real,imaginario;
+	float ang, mod;
+	printf("Que operacion desea realizar:\n Elija 1 si desea pasar de Polar a Rectangular\n Elija 2 si desea pasar de Rectangular a Polar\n");
+	scanf("%d",&e);
+	if (e == 1){
+	printf("Establezca un valor del modulo: ");
+	scanf ("%f", &mod);
+	printf("Establezca un valor del angulo: ");
+	scanf ("%f", &ang);
+	convPol_Rec (mod,ang,&real,&imaginario);
+	printf("La conversion de Polar a Rectangular fue:\n\n");
+	printf("Nª real = %f", real);
+	printf("\nNª imaginario = %f", imaginario);
+	}
+	else if (e==2){
+		printf("Establezca el valor del numero real: ");
+		scanf ("%f", &real);
+		printf("Establezca el valor del numero imaginario: ");
+		scanf ("%f", &imaginario);
+		convRec_Pol (real,imaginario,&mod,&ang);
+		printf("La conversion de Rectangular a Polar fue:\n\n");
+		printf("Nª modulo = %f", mod);
+		printf("\nNª Angulo = %f", ang);
+	}
+	else {}
+	return 0;
+}
+
---
+++ Ejercicio TP 6 2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+using namespace std;
+int resol(float a, float b, float c,float *x1,float *x2){
+*x1 = (-b + sqrt(b*b-4*a*c))/(2*a);
+*x2 = (-b - sqrt(b*b-4*a*c))/(2*a);
+}
+int main() {
+	float A, B, C, x1, x2;
+	printf ("Establezca un valor para A : ");
+	scanf ("%f", &A);
+	printf ("Establezca un valor para B : ");
+	scanf ("%f", &B);
+	printf ("Establezca un valor para C : ");
+	scanf ("%f", &C);
+	resol(A,B,C,&x1,&x2);
+	
+	printf ("Las Raices son:\n\n");
+	printf("x1 = %f",x1); 
+	printf("\nx2 = %f",x2);
+	return 0;
+}
+
